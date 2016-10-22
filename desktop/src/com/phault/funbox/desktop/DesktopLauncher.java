31c06ef865079f9736e66c0da31f6d47ac66a05e
Casper Christiansen <casper.developer@gmail.com>
2016-10-22 19:40:20 +0200

    Added new icons

--- desktop/src/com/phault/funbox/desktop/DesktopLauncher.java
+++ desktop/src/com/phault/funbox/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.phault.funbox.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.phault.funbox.Funbox;
@@ -7,8 +8,12 @@ import com.phault.funbox.Funbox;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Funbox";
 		config.width = 1280;
 		config.height = 720;
+		config.addIcon("icon_128.png", Files.FileType.Internal);
+		config.addIcon("icon_64.png", Files.FileType.Internal);
+		config.addIcon("icon_32.png", Files.FileType.Internal);
 		new LwjglApplication(new Funbox(), config);
 	}
 }
